304d05b0c762cae9578702441f6f88ac0ca04e5e
olgaaktas <binpolyglotte@gmail.com>
2021-03-26 16:27:42 +0300

    factory method

--- src/main/java/io/github/olgaak/springapp/ClassicalMusic.java
+++ src/main/java/io/github/olgaak/springapp/ClassicalMusic.java
@@ -1,6 +1,12 @@
 package io.github.olgaak.springapp;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){};
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
     @Override
     public String getSong() {
         return "Symphony N9";

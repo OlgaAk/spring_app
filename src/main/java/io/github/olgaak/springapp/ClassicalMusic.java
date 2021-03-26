463bf69d61e98359dceec01f9914021be8c8800f
olgaaktas <binpolyglotte@gmail.com>
2021-03-26 17:04:41 +0300

    annotation @qualifier to specify which class implementing interface is needed

--- src/main/java/io/github/olgaak/springapp/ClassicalMusic.java
+++ src/main/java/io/github/olgaak/springapp/ClassicalMusic.java
@@ -2,7 +2,7 @@ package io.github.olgaak.springapp;
 
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class ClassicalMusic implements Music{
     private ClassicalMusic(){};
 
--- src/main/java/io/github/olgaak/springapp/MusicPlayer.java
+++ src/main/java/io/github/olgaak/springapp/MusicPlayer.java
@@ -1,6 +1,7 @@
 package io.github.olgaak.springapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 @Component
 public class MusicPlayer {
     @Autowired
+    @Qualifier("classicalMusic")
     private Music music;
     private List<Music> musicList;
     private int volume;
